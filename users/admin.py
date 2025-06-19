13ec479474bb1259f252be4e7234d0123991c16a
Ankur Paudel <ankur@paudel.me>
2025-06-19 10:47:10 +0100

    feat: add user/group models to admin site

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group as _DjangoGroup
+from django.contrib.auth.admin import UserAdmin as _DjangoUserAdmin, GroupAdmin as _DjangoGroupAdmin
+
+from users.models import User
 
 # Register your models here.
+# admin.site.unregister(_DjangoUser)
+admin.site.unregister(_DjangoGroup)
+
+
+@admin.register(User)
+class UserAdmin(_DjangoUserAdmin):
+    pass
+
+
+@admin.register(_DjangoGroup)
+class GroupAdmin(_DjangoGroupAdmin):
+    pass
