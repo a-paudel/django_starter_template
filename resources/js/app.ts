a4adb8078187c16f9aa8b737e6cbd8af75b62793
Ankur Paudel <ankur@paudel.me>
2025-06-19 10:29:27 +0100

    deps: install alpinejs

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,3 +1,8 @@
 // Add this at the beginning of your app entry.
 import "vite/modulepreload-polyfill";
 import "../css/main.css";
+import Alpine from "alpinejs";
+
+// @ts-ignore
+window.Alpine = Alpine;
+Alpine.start();
